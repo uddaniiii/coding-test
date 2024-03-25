625817a7e947312447d72a50e1fa3f99f056007f
단유 <93571932+uddaniiii@users.noreply.github.com>
2024-03-25 19:53:25 +0900

    [level 0] Title: 세 개의 구분자, Time: 14.28 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,5 +1,7 @@
 def solution(myStr):
-    for s in myStr:
-        if s=='a' or s=='b' or s=='c':
-            myStr=myStr.replace(s,' ')
+
+    if 'a' in myStr or 'b' in myStr or 'c' in myStr:
+        myStr=myStr.replace('a',' ')
+        myStr=myStr.replace('b',' ')
+        myStr=myStr.replace('c',' ')
     return myStr.split() if myStr.split() else ["EMPTY"]
