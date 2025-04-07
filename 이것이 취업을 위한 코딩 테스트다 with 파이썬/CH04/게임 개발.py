e0df038dfbd64c8175543088807786517fb07625
uddaniiii <danyou2260@gmail.com>
2025-04-07 21:18:36 +0900

    Title: 게임 개발

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/CH04/게임 개발.py
@@ -0,0 +1,48 @@
+N,M=map(int,input().split())
+x,y,d=map(int,input().split())
+
+grid = [list(map(int, input().split())) for _ in range(N)]
+# print(map)
+
+bool_map=[[bool(int(x)) for x in row] for row in grid]
+# print(bool_map)
+
+directions={0: [-1, 0], 1: [0, 1], 2: [1, 0], 3: [0, -1]}
+
+bool_map[x][y]=True
+cnt,res=0,1
+nd=0
+while True:
+    # print(f"x, {x}, y: {y}, d: {d}")
+    if d==0:
+        nd=3
+    else:
+        nd=d-1
+
+    # print(x, directions[nd][0])
+    nx=x+directions[nd][0]
+    ny=y+directions[nd][1]
+    # print(f"nx: {nx}, ny: {ny}, d: {d}, nd: {nd}")
+    d=nd
+
+    if bool_map[nx][ny]==False:
+        bool_map[nx][ny]=True
+        res+=1
+        # print(f"res: {res}")
+        x,y=nx,ny
+        cnt=0
+    else:
+        cnt+=1
+
+    if cnt==4:
+        nx=x+directions[(d+2)%4][0]
+        ny=y+directions[(d+2)%4][1]
+        if bool_map[nx][ny]==True:
+            # print(bool_map)
+            if grid[nx][ny]==1:
+                break
+            else:
+                x,y=nx,ny
+        cnt=0
+
+print(res)
