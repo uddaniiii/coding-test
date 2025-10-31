2ae3a8bc3a42d5100f4dd4bf967c4d91a30d1ae9
단유 <93571932+uddaniiii@users.noreply.github.com>
2025-10-31 09:55:30 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+import sys
+
+while True:
+    line = sys.stdin.readline().rstrip()
+    if line == '.':
+        break
+
+    stack = []
+    balanced = True 
+
+    for ch in line:
+        if ch == '(' or ch == '[':
+            stack.append(ch)
+        elif ch == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                balanced = False
+                break
+        elif ch == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                balanced = False
+                break
+
+    if balanced and not stack:
+        print("yes")
+    else:
+        print("no")
